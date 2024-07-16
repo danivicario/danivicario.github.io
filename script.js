baa46b3b62a3657a81130673398954d617eb862c
Dani Vicario <dani@enkitek.eu>
2024-07-16 19:00:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ setTimeout(() => {
   let opacity = 1;
 
   const intervalID = setInterval(() => {
-    opacity -= 0.005 / 3;
+    opacity -= 0.005 / 2;
     document.querySelector("#matterjs-canvas-container").style.opacity =
       opacity;
 
